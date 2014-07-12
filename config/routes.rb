10434b7994c742c092267cfae6571fb5ccc01a1b
smangioni <smangioni@gmail.com>
2014-07-12 12:31:57 -0300

    Adding crud comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :comments
+
   get 'users/new'
   post 'users' => 'users#create'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140712020925) do
+ActiveRecord::Schema.define(version: 20140712151158) do
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
